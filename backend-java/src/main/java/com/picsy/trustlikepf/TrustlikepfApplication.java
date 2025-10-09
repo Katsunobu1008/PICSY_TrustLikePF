04d8187152f870ef25d8a6d4dd06ec98c4f40eda
Katsunobu1008 <katsunobu1008@gmail.com>
2025-10-10 04:43:28 +0900

    TrustlikepfApplication.java（スケジューリングを有効化）

--- backend-java/src/main/java/com/picsy/trustlikepf/TrustlikepfApplication.java
+++ backend-java/src/main/java/com/picsy/trustlikepf/TrustlikepfApplication.java
@@ -3,12 +3,12 @@ package com.picsy.trustlikepf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;   // ★追加
 
 @SpringBootApplication
+@EnableScheduling   // ★追加
 public class TrustlikepfApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TrustlikepfApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(TrustlikepfApplication.class, args);
+  }
 }
