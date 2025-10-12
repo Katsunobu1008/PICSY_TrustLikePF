2ba02daf690223613e1665f4f87ea1e0901545f6
Katsunobu1008 <katsunobu1008@gmail.com>
2025-10-13 00:30:08 +0900

    ルーターに追加

--- frontend-vue/src/router/index.js
+++ frontend-vue/src/router/index.js
@@ -1,29 +1,14 @@
 // frontend-vue/src/router/index.js
-// 役割: ルーティングと、URLクエリ ?actor=UUID -> ストアへのセット
-
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import { setActor, startPolling } from '../stores/power'
+import AdminView from '../views/AdminView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', name: 'home', component: HomeView },
-    { path: '/about', name: 'about', component: AboutView },
-    {
-      path: '/admin',
-      name: 'admin',
-      component: () => import('../components/admin/AdminPanel.vue'),
-    },
+    { path: '/admin', name: 'admin', component: AdminView },
   ],
-})
-
-router.beforeEach((to, from, next) => {
-  const actor = to.query.actor
-  if (actor) setActor(actor)
-  startPolling()
-  next()
 })
 
 export default router
