1a914181a8fbdd5d0b68c65ce1e8ba77ef1cb92a
Katsunobu1008 <katsunobu1008@gmail.com>
2025-10-12 15:07:17 +0900

    フロントがボタンの活性/非活性や不足メッセージを出せるよう、判定APIを用意するための、AffordanceController.javaを作成

---
+++ backend-java/src/main/java/com/picsy/trustlikepf/api/AffordanceController.java
@@ -0,0 +1,28 @@
+// AffordanceController.java
+package com.picsy.trustlikepf.api;
+
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.picsy.trustlikepf.api.dto.AffordanceResponse;
+import com.picsy.trustlikepf.domain.service.AffordanceService;
+
+@RestController
+@RequestMapping("/api/affordance")
+public class AffordanceController {
+
+    private final AffordanceService afford;
+
+    public AffordanceController(AffordanceService afford) { this.afford = afford; }
+
+    @GetMapping("/actors/{actorId}/posts/{postId}")
+    public ResponseEntity<AffordanceResponse> get(@PathVariable UUID actorId,
+                                                  @PathVariable UUID postId){
+        return ResponseEntity.ok(afford.forActorAndPost(actorId, postId));
+    }
+}
